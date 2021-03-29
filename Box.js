7ee4118f73bc4b39e758e048660e3318f8ae657e
AaronSunil <72694245+AaronSunil@users.noreply.github.com>
2021-03-29 08:51:24 +0530

    Add files via upload

--- Box.js
+++ Box.js
@@ -11,10 +11,8 @@ class Box{
         World.add(world, this.body);
       }
       display(){
-        var angle = this.body.angle;
         push();
-        translate(this.body.position.x, this.body.position.y);
-        rotate(angle);
+        translate(this.body.position.x,this.body.position.y);
         pop();
       }
 }
